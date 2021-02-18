1994255d626bd208f63dccebb37f1a92832a5f08
Varad0119 <78159099+Varad0119@users.noreply.github.com>
2021-02-17 22:43:13 -0500

    Deleted

--- lab1_part2.R
+++
@@ -1,7 +0,0 @@
-setwd("/Users/varadpathak/Desktop/ITWS 6600 Data Analytics/Lab 1")
-EPI_data<-read.csv("2010EPI_data.csv")
-View(EPI_data)
-
-#For reading Excel files, readxl package can be used
-#install.packages("readxl")
-colnames(EPI_data)<-EPI_data[1,]
